99572cc8b79986b78463f290325deb93b0283ef3
Harish Jadhav <harishjadhav675@gmail.com>
2023-08-05 18:57:25 +0530

    install mongoose and connect to the db

---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+
+const  mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1/habitTracker_development');
+
+const db =  mongoose.connection;
+
+db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
+
+db.once('open',function(){
+       
+       console.log('connected to Database:: MongoDB');
+});
+
+module.exports = db;
